f1cf017cf602f2f9ac9bb6f0a63c39601ff8e4c5
Alexis Wanja <alexis.wanja@student.moringaschool.com>
2022-02-07 08:18:44 +0300

    declare upvotes and downvotes values

---
+++ src/app/quotes/quotes.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quotes',
+  templateUrl: './quotes.component.html',
+  styleUrls: ['./quotes.component.css']
+})
+export class QuotesComponent implements OnInit {
+
+  upvotes = 0;
+  downvotes = 0;
+  isComplete= true;
+
+  quotes: Quotes[]=[
+
+    new Quotes(0,'Albert','Education','“Try not to become a man of success, but rather try to become a man of value.”',0,0,'Willard'),
+    new Quotes(1,'Albert','Education','“Try not to become a man of success, but rather try to become a man of value.”',0,0,'Willard'),
+    new Quotes(2,'Albert','Education','“Try not to become a man of success, but rather try to become a man of value.”',0,0,'Willard'),
+    new Quotes(3,'Albert','Education','“Try not to become a man of success, but rather try to become a man of value.”',0,0,'Willard'),
+
+  ]
+  getCurrentId(){
+    return this.quotes.length +1
+  }
+  newQuote(quote:any) {
+    quote.id = this.getCurrentId();
+    quote.author= quote.author;
+    quote.title=quote.title;
+    quote.quote=quote.quote;
+    quote.submitedBy=quote.submitedBy;
+    this.quotes.unshift(quote)
+  }
+  upVote(quote:any){
+    quote.upvote= quote.upvote+1;
+    console.log('quote.upvote')
+  }
+  downVote(quote:any){
+    quote.downvote = quote.downvote+1;
+  }
+  quoteDelete(isComplete:any, index:any){
+    if (isComplete) {
+      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].quote}?`)
+
+      if (toDelete){
+        this.quotes.splice(index,1)
+      }
+    }
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
