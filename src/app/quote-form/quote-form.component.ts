b4ebef3faa855dc80578448ffd3c873f728c332b
Alexis Wanja <alexis.wanja@student.moringaschool.com>
2022-02-07 08:39:26 +0300

    assign values to new Quotes

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteFormComponent implements OnInit {
 
+  newQuote = new Quotes(0,'','','',0,0,'');
+
+  @Output() addQuote= new EventEmitter<Quotes>();
+
+    submitQuote(){
+      console.log(this.newQuote)
+      this.addQuote.emit(this.newQuote);
+      
+    }
   constructor() { }
 
   ngOnInit(): void {
